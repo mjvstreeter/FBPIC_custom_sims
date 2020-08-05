19d5f0ff7c53e352a906ee919a7a01f9a5d94ac4
Matthew Streeter <22773109+mjvstreeter@users.noreply.github.com>
2020-08-05 11:53:04 +0100

    Updated installation config

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FBPIC_custom_sims
  Custom scripts for additional FBPIC capabilities
+
+clone and then from the base directory containing this readme type
+pip install .
+
+Dependencies are fbpic and openpmd viewer (version>=1.0.0)
--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
+import fbpic_custom_sims
 
-setup(name='FBPIC_custom_sims',
+setup(name='fbpic_custom_sims',
       version='0.0.1',
       packages=find_packages(),
       zip_safe=False,
